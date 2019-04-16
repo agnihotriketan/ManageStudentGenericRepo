6c0446514605b748b24e028f32905a7c19791693
agnihotriketan <Ketan$123>
2019-04-16 19:26:19 +0530

    changes

--- StudentManagment/StudentManagment/GenericRepo/GenericRepository.cs
+++ StudentManagment/StudentManagment/GenericRepo/GenericRepository.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Threading.Tasks;
-using System.Web;
 
 namespace StudentManagment.GenericRepo
 {
